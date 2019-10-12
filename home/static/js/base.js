706e83d2af8803333b98acdc00c31d1983022ecc
shihyuuuuuuu <www.elmo20816@gmail.com>
2019-10-12 18:59:31 +0800

    Add RWD hamburger menu

--- home/static/js/base.js
+++ home/static/js/base.js
@@ -6,5 +6,16 @@ $(document).ready(function() {
     $('.ui.fluid.card .image').dimmer({
         on: 'hover'
     });
-});
 
+    $('.hamburger').click(function(e){
+		$menu = $(this).parent();
+		if(!$(this).hasClass('active')) {
+			$(this).addClass('active');
+			$menu.addClass('open');
+		} else {
+			$(this).removeClass('active');
+			$menu.removeClass('open');
+		}
+		e.preventDefault();
+	});
+});
