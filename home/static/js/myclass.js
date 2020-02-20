52ba56de6e4bcc48cde1454aeae517e889812222
shihyuuuuuuu <www.elmo20816@gmail.com>
2020-02-20 14:48:30 +0800

    Do some modify on 'myclass' page

--- home/static/js/myclass.js
+++ home/static/js/myclass.js
@@ -1,40 +1,43 @@
 $(document).ready(function() {
     $('#hamburger .item:nth-child(-n+3)').css('display','none'); 
-    $('.myclass .menu a').removeClass('active');
-    $('.myclass .menu a:first-child').addClass('active');
+    $('#myclass .menu a').removeClass('active');
+    $('#myclass .menu a:first-child').addClass('active');
     $.get({
         url: 'bulletin',
         success: function(response){
-            $('.myclass .segment').html(response)
+            $('#myclass .segment').html(response)
             bulletin_js()
         },
     });
 
+    $('#myclass .ui.dropdown').dropdown();
+
     // Use the left menu button to change the content of the right area
-    $('.myclass .tabular.menu a').click(function(){
+    $('#myclass .tabular.menu a').click(function(){
         var page_click
-        $('.myclass .menu a').removeClass('active');
+        $('#myclass .menu a').removeClass('active');
         // Get the page the user clicked on
+        // Todo: Add ids to the items and select by ids
         switch($(this).children().attr("class").split(' ')[0]) {
             case 'tasks':
                 page_click = 'bulletin'
-                $('.myclass .menu a:first-child').addClass('active');
+                $('#myclass .menu a:first-child').addClass('active');
                 break;
             case 'pencil':
                 page_click = 'pretest'
-                $('.myclass .menu a:nth-child(2)').addClass('active');
+                $('#myclass .menu a:nth-child(2)').addClass('active');
                 break;
             case 'comments':
                 page_click = 'discussion'
-                $('.myclass .menu a:nth-child(3)').addClass('active');
+                $('#myclass .menu a:nth-child(3)').addClass('active');
                 break
             case 'table':
                 page_click = 'score'
-                $('.myclass .menu a:nth-child(4)').addClass('active');
+                $('#myclass .menu a:nth-child(4)').addClass('active');
                 break
             default:
                 page_click = 'bulletin'
-                $('.myclass .menu a:first-child').addClass('active');
+                $('#myclass .menu a:first-child').addClass('active');
                 break
         }
 
@@ -42,7 +45,7 @@ $(document).ready(function() {
         $.get({
             url: page_click,
             success: function(response){
-                $('.myclass .segment').html(response)
+                $('#myclass .segment').html(response)
                 // JavaScript script must be run after HTML string is inserted.
                 switch(page_click) {
                     case 'tasks':
